d971568ff4a11c13b091fcae751c8287ea319a0e
Ashil <ashilkp2003@gmail.com>
2024-09-02 13:40:01 +0530

    Update pgm.c

--- lab_7/pgm.c
+++ lab_7/pgm.c
@@ -1,10 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include <ctype.h>
+#include<ctype.h>
 
 char s[100], post[100], stack[100], queue[100];
 int top = -1, j = 0, front = -1;
+
+struct Expression {
+    char op;
+    char arg1;
+    char arg2;
+    char result;
+};
+
+struct Expression exprTable[100];
+int exprCount = 0;
 
 int precedence(char c){
     if(c == '+' || c == '-'){
@@ -22,84 +32,111 @@ int precedence(char c){
 }
 
 void postfix(){
-    top = -1;
-    j = 0;
     int n = strlen(s), i = 0;
     while(s[i] != '\0'){
         if(s[i] == '('){
-            stack[++top] = s[i];
+            top++;
+            stack[top] = s[i];
         }
         else if(isalpha(s[i])){
             post[j++] = s[i];
         }
         else if(precedence(s[i])){
-            while(top != -1 && precedence(stack[top]) >= precedence(s[i])){
+            while(precedence(stack[top]) >= precedence(s[i])){
                 post[j++] = stack[top--];
             }
-            stack[++top] = s[i];
+            top++;
+            stack[top] = s[i];
         }
         else if(s[i] == ')'){
             while(stack[top] != '('){
                 post[j++] = stack[top--];
             }
-            top--; // pop '('
+            top--;
         }
         i++;
     }
-    while(top != -1){
+    while(top!=-1){
         post[j++] = stack[top--];
     }
-    post[j] = '\0'; // Null terminate the postfix expression
+    post[j] = '\0';
+}
+
+int isCommonSubexpression(char op, char arg1, char arg2, char* result){
+    for(int i = 0; i < exprCount; i++){
+        if(exprTable[i].op == op && exprTable[i].arg1 == arg1 && exprTable[i].arg2 == arg2){
+            *result = exprTable[i].result;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void addExpression(char op, char arg1, char arg2, char result){
+    exprTable[exprCount].op = op;
+    exprTable[exprCount].arg1 = arg1;
+    exprTable[exprCount].arg2 = arg2;
+    exprTable[exprCount].result = result;
+    exprCount++;
 }
 
 void main(){
     int i = 0;
     char ind = '1';
+    
+    FILE *fp,*fp1,*fp2,*fp3;
+    fp = fopen( "3addr.txt","w");
+    fp1 = fopen( "quadraple.txt","w");
+    fp2 = fopen( "triple.txt","w");
+    fprintf(fp,"Three Address\n");
+    fprintf(fp1,"Quadruple\nOP\tO1\tO2\tRES\n");
+    fprintf(fp2,"Triple\nIN\tOP\tO1\tO2\n");
+    fp3=fopen( "input.txt","r");
 
-    FILE *fp, *fp1, *fp2, *fp3;
-    fp = fopen("3addr.txt", "w");
-    fp1 = fopen("quadraple.txt", "w");
-    fp2 = fopen("triple.txt", "w");
-    fp3 = fopen("input.txt", "r");
-
-    if (!fp || !fp1 || !fp2 || !fp3) {
-        printf("Error opening file\n");
-        exit(1);
-    }
-
-    fprintf(fp, "Three Address\n");
-    fprintf(fp1, "Quadruple\nOP\tO1\tO2\tRES\n");
-    fprintf(fp2, "Triple\nIN\tOP\tO1\tO2\n");
-
-    while (fscanf(fp3, "%s", s) != EOF) {
-        j = 0;  // Reset postfix index for each new expression
+    while((fscanf(fp3,"%s",s)) != EOF){
         postfix();
-        printf("Infix: %s\nPostfix: %s\n", s, post);
-
-        front = -1; // Reset front for the next expression
-        i = 0;      // Reset index for reading from postfix
-
-        while (post[i] != '\0') {
-            if (precedence(post[i])) {
+        printf("Infix : %s \nPostfix : %s\n", s, post);
+        i = 0;
+        while(post[i] != '\0'){
+            if(precedence(post[i])){
                 char a = queue[front--];
                 char b = queue[front--];
-                if (isalpha(a) && isalpha(b)) {
-                    fprintf(fp, "t%c = %c %c %c\n", ind, b, post[i], a);
-                    fprintf(fp1, "%c\t%c\t%c\tt%c\n", post[i], b, a, ind);
-                } else if (isalpha(b)) {
-                    fprintf(fp, "t%c = %c %c t%c\n", ind, b, post[i], a);
-                    fprintf(fp1, "%c\t%c\tt%c\tt%c\n", post[i], b, a, ind);
-                } else if (isalpha(a)) {
-                    fprintf(fp, "t%c = t%c %c %c\n", ind, b, post[i], a);
-                    fprintf(fp1, "%c\tt%c\t%c\tt%c\n", post[i], b, a, ind);
-                } else {
-                    fprintf(fp, "t%c = t%c %c t%c\n", ind, b, post[i], a);
-                    fprintf(fp1, "%c\tt%c\tt%c\tt%c\n", post[i], b, a, ind);
+                char res;
+                
+                if(isCommonSubexpression(post[i], b, a, &res)){
+                    front++;
+                    queue[front] = res;
                 }
-                fprintf(fp2, "%c\t%c\t%c\t%c\n", ind, post[i], b, a);
-                queue[++front] = ind++; // Push result back to the queue
-            } else {
-                queue[++front] = post[i]; // Push operand to the queue
+                else{
+                    if(isdigit(a) && isdigit(b)){
+                        fprintf(fp,"t%c = t%c %c t%c\n", ind, b, post[i], a);
+                        fprintf(fp1,"%c\tt%c\tt%c\tt%c\n", post[i], b, a, ind);
+                    }
+                    else if(isdigit(b)){
+                        fprintf(fp,"t%c = t%c %c %c\n", ind, b, post[i], a);
+                        fprintf(fp1,"%c\tt%c\t%c\tt%c\n", post[i], b, a, ind);
+                    }
+                    else if(isdigit(a)){
+                        fprintf(fp,"t%c = %c %c t%c\n", ind, b, post[i], a);
+                        fprintf(fp1,"%c\t%c\tt%c\tt%c\n", post[i], b, a, ind);
+                    }
+                    else{
+                        fprintf(fp,"t%c = %c %c %c\n", ind, b, post[i], a);
+                        fprintf(fp1,"%c\t%c\t%c\tt%c\n", post[i], b, a, ind);
+                    }
+                    fprintf(fp2,"%c\t%c\t%c\t%c\n", ind, post[i], b, a);
+                    
+                    // Add the new expression to the table
+                    addExpression(post[i], b, a, ind);
+                    
+                    front++;
+                    queue[front] = ind;
+                    ind++;
+                }
+            }
+            else{
+                front++;
+                queue[front] = post[i];
             }
             i++;
         }
