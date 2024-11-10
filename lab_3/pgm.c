0dcf416cfe762b7821c437ddbd0300eab1476a33
Eeva Eldhose <97470217+Eeva3003@users.noreply.github.com>
2024-11-10 17:02:37 +0530

    Update pgm.c

--- lab_3/pgm.c
+++ lab_3/pgm.c
@@ -1,8 +1,17 @@
+//:Construct a recursive descent parser for an expression according to the grammar below.
+/*
+E → TE’ 
+E’ → +TE’- TE’/ε 
+T → FT’ 
+T’ → *FT’/ /FT’/ ε
+F → (E) / id
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 
-char input[100];
+char input[100];//declaring input array globally is very important
 int i, err;
 
 void E();
@@ -44,23 +53,26 @@ void Tprime() {
         Tprime();
     }
 }
-
+//final function where the recursive parser concludes
 void F() {
-    if (isalnum(input[i])) {
+    if (isalnum(input[i])) {// if an alphanumeric -- tht is identifier , then incremane to next 
         i++;
-    } else if (input[i] == '(') {
+    } else if (input[i] == '(') { //or it might be a open paranthesis which again calls the initial e() function
         i++;
         E();
-        if (input[i] == ')') {
+        if (input[i] == ')') { //after which if closing bracket is encountered continue i++ else print error 
             i++;
         } else {
             err = 1;
         }
-    } else {
+    } else {// else again error
         err = 1;
     }
 }
 
+//main function just takes input , add the '\0 ' and calls the first E() fucntion
+//it then checks if i is the last element of input and err is 0 
+// if yes then accepted else false
 int main() {
     i = 0;
     err = 0;
