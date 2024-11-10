ef5cd8eec7c0515ede4bc378e34c75ec5dd57b99
Eeva Eldhose <97470217+Eeva3003@users.noreply.github.com>
2024-11-10 18:25:49 +0530

    Update pgm.c

--- lab_5/pgm.c
+++ lab_5/pgm.c
@@ -3,7 +3,7 @@
 #include<string.h>
 #include <ctype.h>
 
-char s[100], post[100], stack[100], queue[100];
+char s[100]/*to input string*/, post[100]/*to store postfix*/, stack[100]/*to store stack(convert to postfix)*/, queue[100]/*to store queue(output)*/;// all are character array
 int top = -1, j = 0, front = -1;
 
 int precedence(char c){
@@ -17,62 +17,67 @@ int precedence(char c){
 		return 3;
 	}
 	else{
-		return 0;
+		return 0;//shows its not an operator
 	}
 }
 
 void postfix(){
-	int n = strlen(s), i = 0;
-	while(s[i] != '\0'){
-		if(s[i] == '('){
+	int n = strlen(s), i = 0;//calculate the length of string
+	while(s[i] != '\0'){//iterate till end of string is reach(while loop)
+		if(s[i] == '('){// if open paranthesis directly insert onto top of stack
 			top++;
 			stack[top] = s[i];
 		}
-		else if(isalpha(s[i])){
-			post[j++] = s[i];
+		else if(isalpha(s[i])){// if alphanumeric add to the psotfix expression
+			post[j++] = s[i];//post increament is done
 		}
-		else if(precedence(s[i])){
-			while(precedence(stack[top]) >= precedence(s[i])){
-				post[j++] = stack[top--];
+		else if(precedence(s[i])){// if an operator 
+			while(precedence(stack[top]) >= precedence(s[i])){// while the precendance of the input sympol is greatre than or equal to one on top of stack
+				post[j++] = stack[top--];//add the stack top onto postfix
 			}
 			top++;
-			stack[top] = s[i];
+			stack[top] = s[i];// at the end the cureent symbol is added to the top of the stack
 		}
-		else if(s[i] == ')'){
-			while(stack[top] != '('){
-				post[j++] = stack[top--];
+		else if(s[i] == ')'){// if its a closing bracket
+			while(stack[top] != '('){// till an opening bracket is observed 
+				post[j++] = stack[top--];// add the elements onto postfix expression
 			}
-			top--;
+			top--;// decrement top of the stack to remove opening bracket
 		}
-		i++;
+		i++;//increament i to set to next symbol
 	}
-	while(top!=-1){
+	while(top!=-1){ // if the stack is not compleletly traveersed empty it onto postfix expression
 		post[j++] = stack[top--];
 	}
-	post[j] = '\0';
+	post[j] = '\0';// add end of stack to postfix expression
 }
 
 void main(){
 	int i = 0;
-	char ind = '1';
+	char ind = '1';// the temporray variable which holds t1, t2 etc hence initialised to one 
 	
-	FILE *fp,*fp1,*fp2,*fp3;
+	FILE *fp,*fp1,*fp2,*fp3;// one for input and three for output
 	fp = fopen( "3addr.txt","w");
 	fp1 = fopen( "quadraple.txt","w");
 	fp2 = fopen( "triple.txt","w");
 	fprintf(fp,"Three Address\n");
 	fprintf(fp1,"Quadruple\nOP\tO1\tO2\tRES\n");
 	fprintf(fp2,"Triple\nIN\tOP\tO1\tO2\n");
-	fp3=fopen( "input.txt","r");
+	fp3=fopen( "input.txt","r");//fopent o open all the text files
 	printf("Input the string: ");
 	
 	postfix(); /*not needed i guess*/
-	while((fscanf(fp3,"%s",s))!=EOF)
+	while((fscanf(fp3,"%s",s))!=EOF)// scan each string till the end if file is reached
 	{
-	postfix();
+	postfix();// call postfix to convert the expression and store it in postfix array
 	printf("Infix : %s \nPostfix : %s\n", s, post);
-	while(post[i] != '\0'){
-		if(precedence(post[i])){
+
+		//PARTIAL OUTPUT INFIX TO POSTFIX CONVERSION
+
+
+		
+	while(post[i] != '\0'){// traverse till the end of postfix array is reached
+		if(precedence(post[i])){// if it is an operator
 			char a = queue[front--];/*retrieve two most recent operands from stack to queue*/
 			char b = queue[front--];
 			if(isdigit(a) && isdigit(b)){
@@ -88,19 +93,19 @@ void main(){
 				fprintf(fp1,"%c\t%c\tt%c\tt%c\n", post[i], b, a, ind);
 			}
 			else{
-				fprintf(fp,"t%c = %c %c %c\n", ind, b, post[i], a);
-				fprintf(fp1,"%c\t%c\t%c\tt%c\n", post[i], b, a, ind);
+				fprintf(fp,"t%c = %c %c %c\n", ind, b, post[i], a);// mostly used when all are variabels
+				fprintf(fp1,"%c\t%c\t%c\tt%c\n", post[i], b, a, ind);// in quadruple only result (index needs t)
 			}
 			fprintf(fp2,"%c\t%c\t%c\t%c\n", ind, post[i], b, a);/*in triple you always write the number or variable name same as a and b */
-			front++;
+			front++;// increament front first
 			queue[front] = ind;/*ind is used to create unique temporary variable like t1 , t2 , t3*/
-			ind++;
+			ind++;// add the temporary variable onto front of queue and increamnet index
 		}
 		else{
-			front++;
+			front++; // just adding the operands onto the queue
 			queue[front] = post[i];
 		}
-		i++;
+		i++;// increament i at last with eveyrhitng else
 	}
 	}
 }
